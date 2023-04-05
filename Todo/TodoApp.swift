0e963dcec87d4d0923d50e4dd05a232273220afa
ilija.mihajlovic <ilija.mihajlovic@virginpulse.com>
2023-04-05 10:53:24 +0200

    project setup

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Ilija Mihajlovic on 5. 4. 2023..
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
